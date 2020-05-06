a15b19c851888257353c5a09e21506a509e9fe86
lyk19990226 <1272198474@qq.com>
2020-05-06 21:33:09 +0800

    01_小程序的初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,31 @@ Page({
    * 页面的初始数据
    */
   data: {
+      name: 'Lyk',
+      age: 20,
+      students:[
+        {iid: 1001, name:'kobe', age:39},
+        {iid: 1002, name:'james', age:35},
+        {iid: 1003, name:'curry', age:30},
+        {iid: 1004, name:'wade', age:37},
+        {iid: 1005, name:'lyk', age:20}
+      ],
+      counter:0
+  },
+  handleBtnClick() {
+    //1. 错误做法，界面不会刷新近，但是counter是有改变的(即这里不是响应式的)
+      // this.data.counter --
+      console.log('按钮发生了点击'+this.data.counter)
 
+    // 2. this.setData({})
+       this.setData({
+         counter:this.data.counter - 1
+       })
+  },
+  handleSubtraction() {
+      this.setData({
+        counter:this.data.counter + 1
+      })
   },
 
   /**
