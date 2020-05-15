98f374e493e1f6400d70188b9ff9b485f7502625
lyk19990226 <1272198474@qq.com>
2020-05-15 12:20:40 +0800

    09_弹窗，小程序登录流程代码演练

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,69 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast() {//Toast
+    wx.showToast({
+      title: '操作成功',
+      duration: 3000,
+      // icon:'loading',
+      // image:'/assets/icon/icon.jpg' , //自定义图标
+      mask:true,
+      success: function() {
+        console.log('展示弹窗成功')
+      },
+      fail: function() {
+        console.log('展示弹窗失败')
+      }
+    })
+  },
+  handleShowModal() {//对话框
+    wx.showModal({
+      title:'真的需要收藏吗？',
+      content:'我是内容，哈哈哈',
+      cancelColor: 'cancelColor',
+      confirmText:'是的',
+      cancelText:'我再想想',
+      success: function() {
+        console.log('展示对话框成功')
+      },
+      fail: function() {
+        console.log('展示对话框失败')
+      }
+      
+    })
+  },
+  handleShowLoading() {//loading提示框
+    wx.showLoading({
+      title: '加载中',
+      mask: true,
+      success: function() {
+        console.log('展示loading提示框成功')
+      },
+      fail: function() {
+        console.log('展示loading提示框失败')
+      }
+    })
+    setTimeout(function () {
+      wx.hideLoading()
+    }, 2000)
+  },
+  handleShowActionSheet() {//显示操作菜单
+    wx.showActionSheet({
+      itemList: ['发送给朋友','发送到朋友圈','分享到qq'],
+      itemColor:'#ff5700',
+      success (res) {
+        console.log(res.tapIndex)
+      },
+      fail (res) {
+        console.log(res.errMsg)
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function(options) {//用户点击右上角分享
+    return {
+      title:'你好啊，lyk',
+      path: '/pages/about/about',
+      imageUrl:'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg'
+    }
   }
 })
